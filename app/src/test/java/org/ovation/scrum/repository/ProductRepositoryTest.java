d0214eb01869e90ab553f49288b7ce8356c24cca
Arnaud Courtès <arnaud@courtes.fr>
2026-03-27 22:14:46 +0100

    Tests refactoring

--- app/src/test/java/org/ovation/scrum/repository/ProductRepositoryTest.java
+++ app/src/test/java/org/ovation/scrum/repository/ProductRepositoryTest.java
@@ -1,5 +1,6 @@
 package org.ovation.scrum.repository;
 
+import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatException;
 
 public class ProductRepositoryTest {
 
+    private static final ProductId productId = new ProductId();
     private static ProductRepository sut;
 
     @BeforeAll
@@ -17,79 +19,79 @@ public class ProductRepositoryTest {
 
     @Test
     void should_save_a_product_with_no_id() {
-        String name = "name";
-        String description = "description";
-        Product product = new Product(description, name);
+        Product product = getSimpleProduct();
 
         Product result = sut.save(product);
 
         assertThat(result.getProductId()).isNotNull();
-        assertThat(result.getName()).isEqualTo(name);
-        assertThat(result.getDescription()).isEqualTo(description);
+        assertThat(result.getName()).isEqualTo("name");
+        assertThat(result.getDescription()).isEqualTo("description");
     }
 
     @Test
     void should_save_a_product_with_an_existing_id() {
-        String name = "name";
-        String description = "description";
-        Product product = new Product(description, name);
-        ProductId productId = new ProductId();
-        product.setProductId(productId);
-        product.setVersion(new Version());
+        Product product = getSimpleProduct();
+        addProductIdToProduct(product);
+        addVersionToProduct(product);
         product.updateVersion();
 
         Product result = sut.save(product);
 
         assertThat(result.getProductId()).isEqualTo(productId);
-        assertThat(result.getName()).isEqualTo(name);
-        assertThat(result.getDescription()).isEqualTo(description);
+        assertThat(result.getName()).isEqualTo("name");
+        assertThat(result.getDescription()).isEqualTo("description");
     }
 
     @Test
     void should_save_a_new_product_without_version() {
-        String name = "name";
-        String description = "description";
-        Product product = new Product(description, name);
+        Product product = getSimpleProduct();
 
         Product result = sut.save(product);
 
         assertThat(result.getProductId()).isNotNull();
-        assertThat(result.getName()).isEqualTo(name);
-        assertThat(result.getDescription()).isEqualTo(description);
+        assertThat(result.getName()).isEqualTo("name");
+        assertThat(result.getDescription()).isEqualTo("description");
         assertThat(result.getVersion()).isEqualTo(1);
     }
 
     @Test
     void should_save_a_product_with_existing_version() {
-        String name = "name";
-        String description = "description";
-        Product product = new Product(description, name);
-        ProductId productId = new ProductId();
-        product.setProductId(productId);
-        Version version = new Version();
-        product.setVersion(version);
+        Product product = getSimpleProduct();
+        addProductIdToProduct(product);
+        addVersionToProduct(product);
         product.updateVersion();
 
         Product result = sut.save(product);
 
         assertThat(result.getProductId()).isEqualTo(productId);
         assertThat(result.getVersion()).isEqualTo(3);
-        assertThat(result.getName()).isEqualTo(name);
-        assertThat(result.getDescription()).isEqualTo(description);
+        assertThat(result.getName()).isEqualTo("name");
+        assertThat(result.getDescription()).isEqualTo("description");
     }
 
     @Test
     void should_raise_exception_when_product_has_version_equal_to_what_is_already_in_database() {
-        String name = "name";
-        String description = "description";
-        Product product = new Product(description, name);
-        ProductId productId = new ProductId();
-        product.setProductId(productId);
-        Version version = new Version();
-        product.setVersion(version);
+        Product product = getSimpleProduct();
+        addProductIdToProduct(product);
+        addVersionToProduct(product);
 
         assertThatException().isThrownBy(() -> sut.save(product))
                 .isInstanceOf(ProductException.class)
                 .withMessage("Product has wrong version");
+    }
+
+    private static void addVersionToProduct(Product product) {
+        Version version = new Version();
+        product.setVersion(version);
+    }
+
+    private static void addProductIdToProduct(Product product) {
+        product.setProductId(productId);
+    }
+
+    private static @NonNull Product getSimpleProduct() {
+        String name = "name";
+        String description = "description";
+        return new Product(description, name);
     }
 }
